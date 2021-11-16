839b5b061e68566c29178e2f0ef25774afbc5bef
Jingjing Zhuang <jzhuang3219@gmail.com>
2021-11-16 18:24:32 -0500

    fixed some code

--- push.sh
+++ push.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 
-echo -n "You want to push: "
+echo -n "You want to push [file/.]: "
 read input
 git init
 git add $input
 git status
-echo -n "Commit message: "
+echo -n "Enter commit message: "
 read msg
 git commit -m "$msg"
 echo -n "push to [branchName]: "
