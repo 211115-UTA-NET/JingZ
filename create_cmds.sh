1dee8ec72a896e17b166aef67e201a51b1ba0733
Jingjing Zhuang <jzhuang3219@gmail.com>
2021-11-17 10:38:18 -0500

    change some of the code in create_cmds.sh

--- create_cmds.sh
+++ create_cmds.sh
@@ -14,7 +14,7 @@ echo -n "Do you want to create a file in the folder? (y/n) "
 read x
 if [ $x == "Y" ] || [ $x == "y" ]
 then 
-    echo -n "Create a file name"
+    echo -n "Create a file name: "
 	read filename
 	filepath="./$foldername/$filename"
 	if [ -f $filepath ]
@@ -22,22 +22,26 @@ then
 		echo "file already exists!"
 	else
 		touch $filepath
-		
-		#! write contents
-		echo "write a line to file"
+		# write contents
+		echo -n "write a line to file: "
 		read line
-		#! old numbers
 		echo $line > $filepath
+		# old numbers
 		for (( i=1; i<20; i+=2 ))
 		do
 			echo $i >> $filepath
 		done
-		#! use -n to input even numbers in the same line
-		for (( i=2; i<20; i+=2 ))
+		# use -n to input even numbers in the same line
+		for (( i=2; i<=20; i+=2 ))
 		do
-			echo -n "$i " >> $filepath
+			even+=$i
+			if [ $i -lt 20 ]; then
+				even+=", "
+			fi
 		done
-		#! show contents
+		echo -n $even >> $filepath
+		# show contents
+		echo "[Preview of $filename:]"
 		cat $filepath
 	fi
 	
